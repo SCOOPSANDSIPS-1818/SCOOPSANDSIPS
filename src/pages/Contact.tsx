6ad524330d65114afbf465499ae37c33f2b4ff87
SCOOPSANDSIPS-1818 <Savitrisevkani@gmail.com>
2025-11-23 19:52:05 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -100,7 +100,7 @@ const Contact = () => {
               )}
 
               <form
-                action="https://formsubmit.co/hariramaninayan@gmail.com"
+                action="https://formsubmit.co/ranjithnaniramagiri@gmail.com"
                 method="POST"
                 className="space-y-6"
                 onSubmit={() => setIsSubmitted(true)}
